8fdd010dc503962a6ae083e9a88fe148d5ae7a14
wei.sun <sunweijm.sun@gmail.com>
2024-08-05 15:51:55 +0800

    construct map fail when slide and rotate in one place

--- config/cartographer_mapping.lua
+++ config/cartographer_mapping.lua
@@ -5,26 +5,23 @@ include "trajectory_builder.lua"
 options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
-  --map坐标系
-  map_frame = "map",      
-  --通常为发布频率最高的传感器的frame_id，例如imu_link  
-  tracking_frame = "laser_link", 
-  --cartographer发布发布map到published_frame之间的tf;此处设置错误，建图异常；程序正常运行，使用ros2 run tf2_tools view_frames可以看到map-> lidar_link;
-  published_frame = "odom", 
-  --odom坐标系
-  odom_frame = "odom",
-  --设置为true，cartographer会发布一个 odom 坐标系；--程序正常运行，使用ros2 run tf2_tools view_frames可以看到map->odom->lidar_link;
-  provide_odom_frame = false,
-  publish_frame_projected_to_2d = true,
-  --use_pose_extrapolator = true,
-  use_odometry = true, --是否使用odom数据，使用odom数据时需要看在打滑情况下odom数据是否可信
-  use_nav_sat = false,
+  map_frame = "map",    --地图坐标系名字 
+  tracking_frame = "imu_link",  -- 将所有传感器数据转换到这个坐标系下，如果有imu的，就写imu_link，如果没有，就写base_link或者base_footprint. 
+                                -- cartographer会将所有传感器进行坐标变换到tracking_fram坐标系下，每个传感器的频率不一样，imu频率远高于laser的频率，这样做可以减少计算
+  published_frame = "base_footprint",  --cartographer发布发布map到published_frame之间的tf;
+  odom_frame = "odom", --里程计坐标系名字
+  provide_odom_frame = true,-- 是否提供odom的tf, 如果为true,则tf树为map->odom->base_footprint
+                             -- 如果为false tf树为map->base_footprint
+  publish_frame_projected_to_2d = false,
+  use_pose_extrapolator = false, --发布tf时是使用pose_extrapolator的位姿还是前端计算出来的位姿，前端计算的位姿更准
+  use_odometry = true, --是否使用odom数据，如果使用要求一定要有odom的tf, 使用odom数据时需要看在打滑情况下odom数据是否可信
+  use_nav_sat = false, -- 是否使用gps topic形式订阅，不可订阅多个里程计/gps/landmark，要注意做重映射
   use_landmarks = false,
-  --订阅的lidar scan数量
-  num_laser_scans = 1,
+
+  num_laser_scans = 1,  --订阅的lidar scan数量
   num_multi_echo_laser_scans = 0,
-  --扫描分割数量，为1不分割
-  num_subdivisions_per_laser_scan = 1,
+
+  num_subdivisions_per_laser_scan = 1,  --扫描分割数量，为1不分割
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
@@ -37,7 +34,7 @@ options = {
   landmarks_sampling_ratio = 1.,
 }
 
-MAP_BUILDER.use_trajectory_builder_2d = true
+MAP_BUILDER.use_trajectory_builder_2d = true --在这里进行重写会覆盖子文件中的参数
 
 -- 根据激光雷达的性能，最小范围
 TRAJECTORY_BUILDER_2D.min_range = 0.5
@@ -46,7 +43,7 @@ TRAJECTORY_BUILDER_2D.max_range = 30
 -- 无效激光数据设置距离为该数值
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
 -- false,不使用IMU数据
-TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_imu_data = true
 -- true,使用实时回环检测来进行前端的扫描匹配
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true 
 --尽量小点；如果移动距离或旋转过小, 或者时间过短, 不进行地图的更新
