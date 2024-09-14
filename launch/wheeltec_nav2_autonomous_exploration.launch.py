32c3bd90087516df30169ddf05651985d8f332fa
wei.sun <sunweijm.sun@gmail.com>
2024-09-14 17:33:45 +0800

    update nav2 with mapping

--- launch/wheeltec_nav2_autonomous_exploration.launch.py
+++ launch/wheeltec_nav2_autonomous_exploration.launch.py
@@ -36,20 +36,20 @@ def generate_launch_description():
     #         'maps',
     #         'cartographermap2.yaml'))
 
-    # param_file_name = TURTLEBOT3_MODEL + '.yaml'
-    # param_dir = LaunchConfiguration(
-    #     'params_file',
-    #     default=os.path.join(
-    #         get_package_share_directory('turtlebot3_navigation2'),
-    #         'param',
-    #         param_file_name))
+    param_file_name = 'nav2_wheeltec_mini_diff_robot.yaml'
+    param_dir = LaunchConfiguration(
+        'params_file',
+        default=os.path.join(
+            get_package_share_directory('open_source_slam_launch'),
+            'params',
+            param_file_name))
 
     nav2_launch_file_dir = os.path.join(get_package_share_directory('open_source_slam_launch'), 'launch')
 
-    rviz_config_dir = os.path.join(
-        get_package_share_directory('nav2_bringup'),
-        'rviz',
-        'nav2_default_view.rviz')
+    # rviz_config_dir = os.path.join(
+    #     get_package_share_directory('nav2_bringup'),
+    #     'rviz',
+    #     'nav2_default_view.rviz')
     
     # remappings = [('odom', 'odom_combined')]
 
@@ -59,10 +59,10 @@ def generate_launch_description():
         #     default_value=map_dir,
         #     description='Full path to map file to load'),
 
-        # DeclareLaunchArgument(
-        #     'params_file',
-        #     default_value=param_dir,
-        #     description='Full path to param file to load'),
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=param_dir,
+            description='Full path to param file to load'),
 
         DeclareLaunchArgument(
             'use_sim_time',
@@ -73,8 +73,8 @@ def generate_launch_description():
             PythonLaunchDescriptionSource([nav2_launch_file_dir, '/wheeltec_bringup_launch_auto_explore_mapping.py']),
             launch_arguments={
                 # 'map': map_dir,
-                'use_sim_time': use_sim_time
-                # 'params_file': param_dir
+                'use_sim_time': use_sim_time,
+                'params_file': param_dir
                 }.items(),
         ),
 
