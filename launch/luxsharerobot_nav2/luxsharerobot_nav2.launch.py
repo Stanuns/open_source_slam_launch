2a3d016c29595fe25d59eb81cf1e1db7afd9d639
wei.sun <sunweijm.sun@gmail.com>
2025-05-19 10:16:37 +0800

    update open_source_slam_launch for adding PointCloud2 data to costmap

--- launch/luxsharerobot_nav2/luxsharerobot_nav2.launch.py
+++ launch/luxsharerobot_nav2/luxsharerobot_nav2.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
         default=os.path.join(
             get_package_share_directory('map_server_extension'),
             'maps',
-            'luxsharenanjinghall01.yaml')) #AutoExploreMap_16167.yaml
+            'map_lx_suzhouoffice_0429.yaml')) #AutoExploreMap_16167.yaml
 
     param_file_name = 'nav2_params_luxsharerobot.yaml'
     param_dir = LaunchConfiguration(
