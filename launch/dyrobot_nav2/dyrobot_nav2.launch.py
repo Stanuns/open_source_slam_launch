ee1969108ae413bde1d1a42f581f1d8f5a1cbe40
sunwei-ksdy-robot <sunweijm.sun@gmail.com>
2025-11-19 16:34:16 +0800

    dyrobot naviagtion initialize, 定位采用fast-lio2, 底盘采用阿克曼，局部路径规划采用teb

---
+++ launch/dyrobot_nav2/dyrobot_nav2.launch.py
@@ -0,0 +1,296 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Darby Lim
+
+#Wei Sun source from turtlebot3_navigation2: navigation2.launch.py
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.descriptions import ParameterFile
+from nav2_common.launch import RewrittenYaml
+
+# TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(
+            get_package_share_directory('map_server_extension'),
+            'maps',
+            'map_1030.yaml')) #
+
+    param_file_name = 'nav2_params_dyrobot.yaml'
+    nav2_param_dir = LaunchConfiguration(
+        'params_file',
+        default=os.path.join(
+            get_package_share_directory('open_source_slam_launch'),
+            'launch',
+            param_file_name))
+
+    nav2_launch_file_dir = os.path.join(get_package_share_directory('open_source_slam_launch'), 'launch')
+
+    remappings = [('/tf', 'tf'),
+        ('/tf_static', 'tf_static'),
+        ('/cmd_vel','cmd_vel')
+        ]
+
+    # rviz_config_dir = os.path.join(
+    #     get_package_share_directory('nav2_bringup'),
+    #     'rviz',
+    #     'nav2_default_view.rviz')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_dir,
+            description='Full path to map file to load'),
+
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=nav2_param_dir,
+            description='Full path to param file to load'),
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=use_sim_time,
+            description='Use simulation (Gazebo) clock if true'),
+
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource([nav2_launch_file_dir, '/luxsharerobot_bringup_launch.py']),
+        #     launch_arguments={
+        #         'map': map_dir,
+        #         'use_sim_time': use_sim_time,
+        #         'params_file': nav2_param_dir
+        #         }.items(),
+        # ),
+
+        # localization
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            respawn=True,
+            respawn_delay=2.0,
+            parameters=[ParameterFile(
+                        RewrittenYaml(
+                            source_file=nav2_param_dir,
+                            root_key='',
+                            param_rewrites={
+                                'use_sim_time': use_sim_time,
+                                'yaml_filename': LaunchConfiguration('map')
+                            },
+                            convert_types=True),
+                        allow_substs=True)],
+            arguments=['--ros-args', '--log-level', 'info'],
+            remappings=[('/tf', 'tf'),('/tf_static', 'tf_static')]
+        ),
+        # Node(
+        #     package='nav2_amcl',
+        #     executable='amcl',
+        #     name='amcl',
+        #     output='screen',
+        #     respawn=True,
+        #     respawn_delay=2.0,
+        #     parameters=[ParameterFile(
+        #         RewrittenYaml(
+        #             source_file=nav2_param_dir,
+        #             root_key='',
+        #             param_rewrites={
+        #                 'use_sim_time': use_sim_time,
+        #                 'yaml_filename': LaunchConfiguration('map')
+        #             },
+        #             convert_types=True),
+        #         allow_substs=True)],
+        #     arguments=['--ros-args', '--log-level', 'info'],
+        #     remappings=[('/tf', 'tf'),('/tf_static', 'tf_static')]
+        # ),
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_localization',
+            output='screen',
+            arguments=['--ros-args', '--log-level', 'info'],
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': True},
+                        {'node_names': ['map_server']}]
+        ),
+        
+        #navigation
+        Node(
+            package='nav2_controller',
+            executable='controller_server',
+            output='screen',
+            respawn=True,
+            respawn_delay=2.0,
+            parameters=[ParameterFile(
+                RewrittenYaml(
+                    source_file=nav2_param_dir,
+                    root_key='',
+                    param_rewrites={
+                        'use_sim_time': use_sim_time,
+                        'autostart': 'true'
+                    },
+                    convert_types=True),
+                allow_substs=True)],
+            arguments=['--ros-args', '--log-level', 'info'],
+            # remappings=remappings
+            remappings=remappings + [('cmd_vel', 'cmd_vel')]
+        ),
+        Node(
+            package='nav2_smoother',
+            executable='smoother_server',
+            name='smoother_server',
+            output='screen',
+            respawn=True,
+            respawn_delay=2.0,
+            parameters=[ParameterFile(
+                RewrittenYaml(
+                    source_file=nav2_param_dir,
+                    root_key='',
+                    param_rewrites={
+                        'use_sim_time': use_sim_time,
+                        'autostart': 'true'
+                    },
+                    convert_types=True),
+                allow_substs=True)],
+            arguments=['--ros-args', '--log-level', 'info'],
+            remappings=remappings
+        ),
+        Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_server',
+            output='screen',
+            respawn=True,
+            respawn_delay=2.0,
+            parameters=[ParameterFile(
+                RewrittenYaml(
+                    source_file=nav2_param_dir,
+                    root_key='',
+                    param_rewrites={
+                        'use_sim_time': use_sim_time,
+                        'autostart': 'true'
+                    },
+                    convert_types=True),
+                allow_substs=True)],
+            arguments=['--ros-args', '--log-level', 'info'],
+            remappings=remappings
+        ),
+        Node(
+            package='nav2_behaviors',
+            executable='behavior_server',
+            name='behavior_server',
+            output='screen',
+            respawn=True,
+            respawn_delay=2.0,
+            parameters=[ParameterFile(
+                RewrittenYaml(
+                    source_file=nav2_param_dir,
+                    root_key='',
+                    param_rewrites={
+                        'use_sim_time': use_sim_time,
+                        'autostart': 'true'
+                    },
+                    convert_types=True),
+                allow_substs=True)],
+            arguments=['--ros-args', '--log-level', 'info'],
+            remappings=remappings
+        ),
+        Node(
+            package='nav2_bt_navigator',
+            executable='bt_navigator',
+            name='bt_navigator',
+            output='screen',
+            respawn=True,
+            respawn_delay=2.0,
+            parameters=[ParameterFile(
+                RewrittenYaml(
+                    source_file=nav2_param_dir,
+                    root_key='',
+                    param_rewrites={
+                        'use_sim_time': use_sim_time,
+                        'autostart': 'true'
+                    },
+                    convert_types=True),
+                allow_substs=True)],
+            arguments=['--ros-args', '--log-level', 'info'],
+            remappings=remappings
+        ),
+        Node(
+            package='nav2_waypoint_follower',
+            executable='waypoint_follower',
+            name='waypoint_follower',
+            output='screen',
+            respawn=True,
+            respawn_delay=2.0,
+            parameters=[ParameterFile(
+                RewrittenYaml(
+                    source_file=nav2_param_dir,
+                    root_key='',
+                    param_rewrites={
+                        'use_sim_time': use_sim_time,
+                        'autostart': 'true'
+                    },
+                    convert_types=True),
+                allow_substs=True)],
+            arguments=['--ros-args', '--log-level', 'info'],
+            remappings=remappings
+        ),
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_navigation',
+            output='screen',
+            arguments=['--ros-args', '--log-level', 'info'],
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': True},
+                        {'node_names': ['controller_server',
+                                        'smoother_server',
+                                        'planner_server',
+                                        'behavior_server',
+                                        'bt_navigator',
+                                        'waypoint_follower'
+                                        # 'velocity_smoother'
+                                        ]}
+            ]
+        ),
+
+
+        # #Debug
+        # Node(
+        # package = 'cartographer_ros',
+        # executable = 'cartographer_odom_preproc',
+        # name='cartographer_odom_preproc',
+        # parameters = [
+        #     {'use_sim_time': True}]),
+
+        # Node(
+        #     package='rviz2',
+        #     executable='rviz2',
+        #     name='rviz2',
+        #     arguments=['-d', rviz_config_dir],
+        #     parameters=[{'use_sim_time': use_sim_time}],
+        #     output='screen'),
+    ])
